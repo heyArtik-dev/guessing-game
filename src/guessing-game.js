997569269830052106857e6c86ea29a341c03d4d
heyArtik-dev <heyArtik@rambler.ru>
2024-08-28 14:12:16 +0300

    feat: one test is falling

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,36 @@
 class GuessingGame {
-    constructor() {}
+
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.currentGuess = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.currentGuess = Math.ceil(min + (min + max) / 2);
     }
 
     guess() {
-
+        return this.currentGuess;
     }
 
     lower() {
-
+        this.max = this.currentGuess + 1;
+        this.updateGuess();
     }
 
     greater() {
-
+        this.min = this.currentGuess;
+        this.updateGuess();
     }
+
+    updateGuess() {
+        if (this.min <= this.max) {
+          this.currentGuess = Math.floor((this.min + this.max) / 2);
+        }
+      }
 }
 
 module.exports = GuessingGame;
