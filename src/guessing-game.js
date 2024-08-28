d1746da6cd8d923bf5d420dba7d0ef6c0f7ac7d4
heyArtik-dev <heyArtik@rambler.ru>
2024-08-28 15:00:39 +0300

    fix: code

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,36 +1,30 @@
 class GuessingGame {
 
-    constructor() {
-        this.min = 0;
-        this.max = 0;
-        this.currentGuess = 0;
-    }
+  constructor() {
+      this.min = 0;
+      this.max = 0;
+      this.currentGuess = 0;
+  }
 
-    setRange(min, max) {
-        this.min = min;
-        this.max = max;
-        this.currentGuess = Math.ceil(min + (min + max) / 2);
-    }
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+    this.currentGuess = Math.ceil((min + max) / 2);
+  }
 
-    guess() {
-        return this.currentGuess;
-    }
+  guess() {
+    return this.currentGuess;
+  }
 
-    lower() {
-        this.max = this.currentGuess + 1;
-        this.updateGuess();
-    }
+  lower() {
+    this.max = this.currentGuess + 1;
+    this.currentGuess = Math.floor((this.min + this.max) / 2);
+  }
 
-    greater() {
-        this.min = this.currentGuess;
-        this.updateGuess();
-    }
-
-    updateGuess() {
-        if (this.min <= this.max) {
-          this.currentGuess = Math.floor((this.min + this.max) / 2);
-        }
-      }
+  greater() {
+    this.min = this.currentGuess;
+    this.currentGuess = Math.floor((this.min + this.max) / 2);
+  }
 }
 
 module.exports = GuessingGame;
